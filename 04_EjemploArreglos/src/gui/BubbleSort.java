2ca9d70b0f2f9d25804c7aba17e68240b1496c37
paulabaal12 <106341373+paulabaal12@users.noreply.github.com>
2023-01-31 09:10:06 -0600

    .

--- 04_EjemploArreglos/src/gui/BubbleSort.java
+++ 04_EjemploArreglos/src/gui/BubbleSort.java
@@ -12,9 +12,9 @@ import java.util.Comparator;
 public class BubbleSort<T> {
 	
 	public void sort(T[] arreglo, Comparator<T> comparador) {
-		for (int i = 0; i < arreglo.length - 1; i++) {
-			for (int j = i + 1; j < arreglo.length; j++) {
-				if (comparador.compare(arreglo[i], arreglo[j]) > 0) {
+		for (int i = 0; i < arreglo.length - 1; i++) { //TIENE DOS CICLOS ; interior y exterior (externo e interno)
+			for (int j = i + 1; j < arreglo.length; j++) { // j inicia desde 1, cuando i se mueva va a ir detrás.
+				if (comparador.compare(arreglo[i], arreglo[j]) > 0) { //Van a comparar, si i es mayor a lo que esta en j, Se hace un intercambio
 					T aux = arreglo[j];
 					arreglo[j] = arreglo[i];
 					arreglo[i] = aux;
@@ -23,3 +23,4 @@ public class BubbleSort<T> {
 		}
 	}
 }
+//LO QUE HACE EL METODO ES ORDENAR BubbleSort
